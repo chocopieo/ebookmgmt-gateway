7439f068d73cba5ae6f3a0c1bebd82cdebb04cf6
chocopieo <ysj1106@gmail.com>
2021-07-24 19:27:25 +0900

    개발 소스 반영

--- src/main/java/ebookmanagement/EbookmgmtGatewayApplication.java
+++ src/main/java/ebookmanagement/EbookmgmtGatewayApplication.java
@@ -1,15 +1,15 @@
-package com.example;
+package ebookmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class EbookmgmtGatewayApplication {
 
     public static ApplicationContext applicationContext;
     public static void main(String[] args) {
-        applicationContext = SpringApplication.run(Application.class, args);
+        applicationContext = SpringApplication.run(EbookmgmtGatewayApplication.class, args);
     }
 
 
